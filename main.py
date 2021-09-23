3e3b33fb5bf0aa3cb2ecaf6b9e21b5503af30f63
HM <hieumdd@gmail.com>
2021-09-23 23:30:02 +0700

    !refactor: Refactored tests

--- main.py
+++ main.py
@@ -1,32 +1,18 @@
-import json
-import base64
-
 from models.models import Voluum
-from broadcast import broadcast
 
 
 def main(request):
-    request_json = request.get_json()
-    message = request_json["message"]
-    data_bytes = message["data"]
-    data = json.loads(base64.b64decode(data_bytes).decode("utf-8"))
+    data = request.get_json()
     print(data)
 
-    if "broadcast" in data:
-        results = broadcast(data)
-    elif "table" in data:
-        job = Voluum.factory(
+    if "table" in data:
+        response = Voluum.factory(
             data["table"],
             data.get("start"),
             data.get("end"),
-        )
-        results = job.run()
+        ).run()
     else:
-        raise NotImplementedError(data)
+        raise ValueError(data)
 
-    responses = {
-        "pipelines": "Voluum",
-        "results": results,
-    }
-    print(responses)
-    return responses
+    print(response)
+    return response
---
+++ test/test_units.py
@@ -0,0 +1,36 @@
+from unittest.mock import Mock
+
+import pytest
+from main import main
+
+
+@pytest.mark.parametrize(
+    "table",
+    [
+        "ReportConversions",
+        "Report",
+        "Offer",
+    ],
+)
+@pytest.mark.parametrize(
+    ("start", "end"),
+    [
+        (None, None),
+        ("2021-09-01", "2021-09-02"),
+    ],
+    ids=("auto", "manual"),
+)
+def test_units(table, start, end):
+    if table == "Offer" and start is None:
+        pytest.skip()
+    data = {
+        "table": table,
+        "start": start,
+        "end": end,
+    }
+    req = Mock(get_json=Mock(return_value=data), args=data)
+    res = main(req)
+    assert res["num_processed"] >= 0
+    if res["num_processed"] > 0:
+        assert res["output_rows"] > 0
+        assert res["num_processed"] == res["output_rows"]
